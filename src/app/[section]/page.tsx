999d8205b477361af28307fe0db843bf7cc991fc
HyacinthFrance <hyacinthfrance666@gmail.com>
2025-05-15 10:25:13 +0700

    edit section

--- src/app/[section]/page.tsx
+++ src/app/[section]/page.tsx
@@ -2,82 +2,82 @@ import { notFound } from "next/navigation";
 import SectionPage from "./section-page";
 
 export type ApData = {
-	status: boolean;
-	location: string;
-	building: string;
-	all: number;
-	maintain: number;
-	down: number;
-	c24: number;
-	c50: number;
-	cmax: number;
-	wlc: number;
+  status: boolean;
+  location: string;
+  building: string;
+  all: number;
+  maintain: number;
+  down: number;
+  c24: number;
+  c50: number;
+  cmax: number;
+  wlc: number;
 };
 
-const getZoneData = async () => {
-	const res = await fetch(
-		"https://67f4bef5cbef97f40d2f310d.mockapi.io/api/apData"
-	);
-	if (!res.ok) {
-		console.log("Fetch Data error");
-		return notFound();
-	}
-	return await res.json();
-};
+// const getZoneData = async () => {
+//   const res = await fetch(
+//     "https://67f4bef5cbef97f40d2f310d.mockapi.io/api/apData"
+//   );
+//   if (!res.ok) {
+//     console.log("Fetch Data error");
+//     return notFound();
+//   }
+//   return await res.json();
+// };
 
-async function Apcount() {
-	const ap: ApData[] = await getZoneData();
-	let maintain: number = 0;
-	let down: number = 0;
-	ap.forEach((overap) => {
-		if (overap.status == true) {
-			maintain += 1;
-		} else {
-			down += 1;
-		}
-	});
-	return { maintain, down };
-}
+// async function Apcount() {
+//   const ap: ApData[] = await getZoneData();
+//   let maintain: number = 0;
+//   let down: number = 0;
+//   ap.forEach((overap) => {
+//     if (overap.status == true) {
+//       maintain += 1;
+//     } else {
+//       down += 1;
+//     }
+//   });
+//   return { maintain, down };
+// }
 
 export default async function Page({
-	params,
+  params,
 }: {
-	params: Promise<{ section: string }>;
+  params: Promise<{ section: string }>;
 }) {
-	const { section } = await params;
+  const { section } = await params;
 
-	if (["faculty", "organization", "dormitory"].includes(section) == false) {
-		console.log("Error: Invalid section");
-		return notFound();
-	}
+  if (["faculty", "organization", "dormitory"].includes(section) == false) {
+    console.log("Error: Invalid section");
+    return notFound();
+  }
 
-	const zoneData: ApData[] = await getZoneData();
-	const apSum = await Apcount();
-	zoneData.forEach((element) => {
-		element.maintain = apSum.maintain;
-		element.down = apSum.down;
-		element.all = apSum.maintain + apSum.down;
-	});
-	// return (
-	// 	<div className="bg-[#FFFEEA] font-sans w-full">
-	// 		<h1 className="text-left font-bold m-4 text-[48px] italic">
-	// 			Zone ที่ {section} ในมหาวิทยาลัยเกษตรศาสตร์
-	// 		</h1>
-	// 		<div className="w-full">
-	// 			{<DataTable column={headTable} data={zoneData} />}
-	// 		</div>
-	// 	</div>
-	// );
+  //const zoneData: ApData[] = await getZoneData();
+  //   const apSum = await Apcount();
+  //   zoneData.forEach((element) => {
+  //     element.maintain = apSum.maintain;
+  //     element.down = apSum.down;
+  //     element.all = apSum.maintain + apSum.down;
+  //   });
+  // return (
+  // 	<div className="bg-[#FFFEEA] font-sans w-full">
+  // 		<h1 className="text-left font-bold m-4 text-[48px] italic">
+  // 			Zone ที่ {section} ในมหาวิทยาลัยเกษตรศาสตร์
+  // 		</h1>
+  // 		<div className="w-full">
+  // 			{<DataTable column={headTable} data={zoneData} />}
+  // 		</div>
+  // 	</div>
+  // );
 
-	const entities = await fetch(
-		`http://localhost:3001/entities/${section}`
-	).then((res) => {
-		if (!res.ok) {
-			throw new Error(`Failed to fetch data for section: ${section}`);
-		}
-		return res.json();
-	});
-	console.log("Entities: ", entities);
-	// return <SectionPage section={section} data={entities} />;
-	return <SectionPage section={section} data={zoneData} />; // Pass the zoneData to SectionPage
+  const entities = await fetch(
+    `http://localhost:3001/entities/${section}`
+  ).then((res) => {
+    if (!res.ok) {
+      throw new Error(`Failed to fetch data for section: ${section}`);
+    }
+    return res.json();
+  });
+  console.log("Entities: ", entities);
+  // return <SectionPage section={section} data={entities} />;
+  return <SectionPage section={section} data={entities} />; // Pass the zoneData to SectionPage
 }
--- src/app/[section]/section-page.tsx
+++ src/app/[section]/section-page.tsx
@@ -5,30 +5,27 @@ import React from "react";
 import { ApData } from "./page";
 
 const columns = [
-	{ accessorKey: "location", header: "Faculty" },
-	{ accessorKey: "all", header: "# AP (overall)" },
-	{ accessorKey: "maintain", header: "# AP (maintain)" },
-	{ accessorKey: "down", header: "# AP (down)" },
-	{ accessorKey: "c24", header: "# Client (2.4GHz)" },
-	{ accessorKey: "c50", header: "# Client (5.0GHz)" },
-	{ accessorKey: "wlc", header: "# WLC" },
+  { accessorKey: "name", header: "Faculty" },
+  { accessorKey: "numAp", header: "# AP (overall)" },
+  { accessorKey: "numCl", header: "# Client" },
 ];
 
 export default function SectionPage({
-	section,
-	data,
+  section,
+  data,
 }: {
-	section: string;
-	data: ApData[];
+  section: string;
+  data: ApData[];
 }) {
-	return (
-		<div className="bg-[#FFFEEA] font-sans w-full p-4 h-auto overflow-y-auto no-scrollbar">
-			<h1 className="text-left font-bold m-4 text-[48px] italic">
-				Zone ที่ {section} ในมหาวิทยาลัยเกษตรศาสตร์
-			</h1>
-			<div className="w-full">
-				<DataTable column={columns} data={data} />
-			</div>
-		</div>
-	);
+  const headerName: string = section;
+  return (
+    <div className="bg-[#FFFEEA] font-sans w-full p-4 h-auto overflow-y-auto no-scrollbar">
+      <h1 className="text-left font-bold m-4 text-[48px] italic">
+        {headerName} ในมหาวิทยาลัยเกษตรศาสตร์
+      </h1>
+      <div className="w-full">
+        <DataTable column={columns} data={data} />
+      </div>
+    </div>
+  );
 }

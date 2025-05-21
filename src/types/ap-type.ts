ab70c2bb8b26ebb35ac356b8acb7e03239a9eab4
Saksorn Malasuttichai <s.malasutt@gmail.com>
2025-05-21 22:52:06 +0700

    fetch data for report form

--- src/types/ap-type.ts
+++ src/types/ap-type.ts
@@ -1,21 +1,26 @@
 type AccessPoint = {
-  ipAddress: string;
-  apName: string;
-  wlc: string;
-  model: string;
-  firmware: string;
-  serial: string;
-  rdmac: string;
-  ethmac: string;
-  apLoc: string;
-  numCli: number;
-  switch: string;
-  eqNum: string;
-  channel: string;
-  rxRate: number;
-  txRate: number;
-  iqd: number;
-  oqd: number;
+	ipAddress: string;
+	apName: string;
+	wlc: string;
+	model: string;
+	firmware: string;
+	serial: string;
+	rdmac: string;
+	ethmac: string;
+	apLoc: string;
+	numCli: number;
+	switch: string;
+	eqNum: string;
+	channel: string;
+	rxRate: number;
+	txRate: number;
+	iqd: number;
+	oqd: number;
 };
 
-export type { AccessPoint };
+type AccessPointName = {
+	id: number;
+	name: string;
+};
+
+export type { AccessPoint, AccessPointName };

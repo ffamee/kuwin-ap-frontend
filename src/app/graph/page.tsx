0bf9d939480647b8e9ade9cbc91f371e6559501b
Saksorn Malasuttichai <s.malasutt@gmail.com>
2025-05-28 15:26:57 +0700

    remove folder graph

--- src/app/graph/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Page() {
-  return (
-    <div className="flex flex-col items-center justify-center p-9 h-auto w-full pb-28">
-      <h1 className="text-4xl font-bold">Hello, World!</h1>
-      <p className="my-4 text-lg">Welcome to my Graph Page!!!!!</p>
-    </div>
-  );
-}

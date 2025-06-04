f66a4037dfd2f633199f8d174bd8219d8d7b7044
Saksorn Malasuttichai <s.malasutt@gmail.com>
2025-06-04 13:16:33 +0700

    edit building overview api to use search params

--- src/app/monitor/[section]/[entity]/[building]/page.tsx
+++ src/app/monitor/[section]/[entity]/[building]/page.tsx
@@ -1,19 +1,31 @@
 import BuildingPage from "./building-page";
+import { notFound } from "next/navigation";
+
+async function getBuildingOverview(
+	section: string,
+	entity: string,
+	building: string
+) {
+	const res = await fetch(
+		`http://localhost:3001/buildings/overview?sec=${section}&entity=${entity}&build=${building}`
+	);
+	if (res.status === 404) {
+		return notFound();
+	}
+	if (!res.ok) {
+		throw new Error(`Failed to fetch building ${building} in entity ${entity}`);
+	}
+	return await res.json();
+}
 
 export default async function Page({
 	params,
 }: {
-	params: Promise<{ building: string }>;
+	params: Promise<{ section: string; entity: string; building: string }>;
 }) {
-	const { building } = await params;
-	const data = await fetch(
-		`http://localhost:3001/buildings/overview/${building}`
-	)
-		.then((res) => res.json())
-		.catch((error) => {
-			console.error("Error fetching building data:", error);
-			throw new Error("Failed to fetch building data");
-		});
+	const { section, entity, building } = await params;
+
+	const data = await getBuildingOverview(section, entity, building);
 
 	return <BuildingPage buildingId={building} data={data} />;
 }

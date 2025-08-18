7675aca33905943a460edd48eba7782e69ffc218
HyacinthFrance <hyacinthfrance666@gmail.com>
2025-08-18 14:39:23 +0700

    update all devices table

--- src/app/monitor/(admin)/device/admin-device-all-table.tsx
+++ src/app/monitor/(admin)/device/admin-device-all-table.tsx
@@ -4,35 +4,14 @@ import AdminDeviceTable from "@/components/table/admin-device-table";
 
 import { ColumnDef } from "@tanstack/react-table";
 
-import { AllApTab } from "@/types/ap-type";
-
 import { DataTableColumnHeader } from "@/components/table/data-table-header";
 import { GetAllConfigurations } from "./device-handler";
+import { ConfigOverview } from "@/types/config-type";
 
 const data = await GetAllConfigurations();
 
 export default function AdminAllDeviceTable() {
-  const columns: ColumnDef<AllApTab>[] = [
-    {
-      accessorKey: "name",
-      header: ({ column }) => (
-        <DataTableColumnHeader column={column} title="Name" />
-      ),
-      filterFn: (row, columnId, value) => {
-        const rowData: string = row.getValue("name");
-        if (!row.original.name) return false;
-        return rowData.toLowerCase().includes(value.toLowerCase());
-      },
-      cell: ({ row }) => {
-        if (!row.original.name) return <div>NaN</div>;
-        const url = "/monitor";
-        return (
-          <Link href={url} className="max-w-xs whitespace-normal break-words">
-            {row.getValue("name")}
-          </Link>
-        );
-      },
-    },
+  const columns: ColumnDef<ConfigOverview>[] = [
     {
       accessorKey: "status",
       header: ({ column }) => (
@@ -41,40 +20,104 @@ export default function AdminAllDeviceTable() {
       enableSorting: false,
       enableColumnFilter: false,
     },
+
     {
       accessorKey: "ip",
       header: ({ column }) => (
         <DataTableColumnHeader column={column} title="IP Address" />
       ),
+      cell: ({ row }) => {
+        if (!row.original.ip.ip) return <div>NaN</div>;
+        return row.original.ip.ip;
+      },
     },
+
     {
-      accessorKey: "location",
+      accessorKey: "name",
       header: ({ column }) => (
         <DataTableColumnHeader column={column} title="Location" />
       ),
-      enableSorting: false,
-    },
-    {
-      id: "totalUser",
-      accessorFn: (row) =>
-        Number(row.numberClient) + Number(row.numberClient_2),
-      header: ({ column }) => (
-        <DataTableColumnHeader column={column} title="Total User" />
-      ),
+      filterFn: (row, columnId, value) => {
+        const rowData: string = row.getValue("name");
+        if (!row.original.location.name) return false;
+        return rowData.toLowerCase().includes(value.toLowerCase());
+      },
       cell: ({ row }) => {
-        const total =
-          Number(row.original.numberClient) +
-          Number(row.original.numberClient_2);
-        return <div>{total}</div>;
+        if (!row.original.location.name) return <div>NaN</div>;
+        const url = "/monitor";
+        return (
+          <Link href={url} className="max-w-xs whitespace-normal break-words">
+            {row.original.location.name}
+          </Link>
+        );
       },
     },
+
     {
-      accessorKey: "wlcActive",
+      accessorKey: "problem",
       header: ({ column }) => (
-        <DataTableColumnHeader column={column} title="WLC" />
+        <DataTableColumnHeader column={column} title="Problem" />
       ),
-      enableSorting: false,
+      cell: ({ row }) => {
+        return row.original.problem;
+      },
     },
+
+    {
+      accessorKey: "client24",
+      header: ({ column }) => (
+        <DataTableColumnHeader column={column} title="Client (2.4GHz)" />
+      ),
+      cell: ({ row }) => {
+        if (!row.original.client24) return <div>-</div>;
+        return row.original.client24;
+      },
+    },
+
+    {
+      accessorKey: "client5",
+      header: ({ column }) => (
+        <DataTableColumnHeader column={column} title="Client (5GHz)" />
+      ),
+      cell: ({ row }) => {
+        if (!row.original.client5) return <div>-</div>;
+        return row.original.client5;
+      },
+    },
+
+    {
+      accessorKey: "client6",
+      header: ({ column }) => (
+        <DataTableColumnHeader column={column} title="Client (6GHz)" />
+      ),
+      cell: ({ row }) => {
+        if (!row.original.client6) return <div>-</div>;
+        return row.original.client6;
+      },
+    },
+
+    // {
+    //   id: "totalUser",
+    //   accessorFn: (row) =>
+    //     Number(row.client24) + Number(row.client5) + Number(row.client6),
+    //   header: ({ column }) => (
+    //     <DataTableColumnHeader column={column} title="#User" />
+    //   ),
+    //   cell: ({ row }) => {
+    //     const total =
+    //       Number(row.original.client24 ?? 0) +
+    //       Number(row.original.client5 ?? 0) +
+    //       Number(row.original.client6 ?? 0);
+    //     return <div>{total}</div>;
+    //   },
+    // },
+    // {
+    //   accessorKey: "wlcActive",
+    //   header: ({ column }) => (
+    //     <DataTableColumnHeader column={column} title="WLC" />
+    //   ),
+    //   enableSorting: false,
+    // },
   ];
 
   return <AdminDeviceTable columns={columns} data={data} />;

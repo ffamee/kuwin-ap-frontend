2ac43e47e65bb7a223d8ea26d5551664e2ca9900
Saksorn Malasuttichai <s.malasutt@gmail.com>
2025-08-25 14:41:22 +0700

    unused var config and remote image pattern

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,27 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+	baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+	...compat.extends("next/core-web-vitals", "next/typescript"),
+	{
+		rules: {
+			"@typescript-eslint/no-unused-vars": [
+				"warn",
+				{
+					args: "all",
+					argsIgnorePattern: "^_",
+					caughtErrors: "all",
+					caughtErrorsIgnorePattern: "^_",
+					destructuredArrayIgnorePattern: "^_",
+					varsIgnorePattern: "^_",
+					ignoreRestSiblings: true,
+				},
+			],
+		},
+	},
 ];
 
 export default eslintConfig;

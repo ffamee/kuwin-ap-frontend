8428455ba21201fe481eb9f80991437a8df68b03
Saksorn Malasuttichai <s.malasutt@gmail.com>
2025-08-25 14:40:45 +0700

    toast error cause

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,77 +2,90 @@
 
 import { Undo2 } from "lucide-react";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
+import { toast } from "sonner";
+import * as React from "react";
 
 export default function Page() {
-  const router = useRouter();
-  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
-    // after login complete, redirect to /pro
-    e.preventDefault();
-    const formData = new FormData(e.currentTarget);
-    const username = formData.get("username");
-    const password = formData.get("password");
+	const params = useSearchParams();
+	const cause = params.get("cause");
+	const router = useRouter();
+	const initialState = React.useRef(true);
+	const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+		// after login complete, redirect to /pro
+		e.preventDefault();
+		const formData = new FormData(e.currentTarget);
+		const username = formData.get("username");
+		const password = formData.get("password");
 
-    try {
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/login`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ username, password }),
-          credentials: "include", // Include cookies in the request
-        }
-      );
+		try {
+			const response = await fetch(
+				`${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/login`,
+				{
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({ username, password }),
+					credentials: "include", // Include cookies in the request
+				}
+			);
 
-      if (response.ok) {
-        // Assume backend sets cookies on successful login
-        console.log("Login successful!");
-        router.push("/");
-      } else {
-        console.log("Login failed. Please check your credentials.");
-      }
-    } catch (error) {
-      console.error("Error during login:", error);
-    }
-  };
+			if (response.ok) {
+				// Assume backend sets cookies on successful login
+				console.log("Login successful!");
+				// router.push("/");
+				router.back();
+			} else {
+				console.log("Login failed. Please check your credentials.");
+			}
+		} catch (error) {
+			console.error("Error during login:", error);
+		}
+	};
 
-  return (
-    <div className="flex flex-col items-center justify-center p-9 h-screen w-screen">
-      <Link
-        href="/"
-        className="absolute top-4 left-4 text-blue-500 hover:underline items-center flex gap-1"
-      >
-        <Undo2 className="inline ml-1" />
-        Back
-      </Link>
-      <form
-        className="flex flex-col items-center justify-center w-3/5 min-w-3xs h-full gap-4"
-        onSubmit={handleLogin}
-      >
-        <h1 className="text-4xl font-bold">Login</h1>
-        <input
-          type="text"
-          placeholder="Username"
-          name="username"
-          className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-          autoComplete="username"
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          name="password"
-          className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-        <button
-          className="w-full p-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition duration-200"
-          name="submit"
-          type="submit"
-        >
-          Login
-        </button>
-      </form>
-    </div>
-  );
+	React.useEffect(() => {
+		if (initialState.current) {
+			if (cause) toast.error("Session expired. Please log in again.");
+			initialState.current = false;
+		}
+	}, [cause]);
+
+	return (
+		<div className="flex flex-col items-center justify-center p-9 h-screen w-screen">
+			<Link
+				href="/"
+				className="absolute top-4 left-4 text-blue-500 hover:underline items-center flex gap-1"
+			>
+				<Undo2 className="inline ml-1" />
+				Back
+			</Link>
+			<form
+				className="flex flex-col items-center justify-center w-3/5 min-w-3xs h-full gap-4"
+				onSubmit={handleLogin}
+			>
+				<h1 className="text-4xl font-bold">Login</h1>
+				<input
+					type="text"
+					placeholder="Username"
+					name="username"
+					className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+					autoComplete="username"
+				/>
+				<input
+					type="password"
+					placeholder="Password"
+					name="password"
+					className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+				/>
+				<button
+					className="w-full p-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition duration-200"
+					name="submit"
+					type="submit"
+				>
+					Login
+				</button>
+			</form>
+		</div>
+	);
 }
